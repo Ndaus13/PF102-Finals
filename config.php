302695c11953e2ad7c47868ed7c5c3136934ba06
Ndaus13 <132029733+Ndaus13@users.noreply.github.com>
2023-12-22 09:18:59 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('sql301.infinityfree.com','if0_35656964','8XCvOxPcv9BGF','if0_35656964_psau_library_db') or die('connection failed');
+
+?>
---
+++ footer.php
@@ -0,0 +1,26 @@
+<section class="footer">
+
+   <div class="box-container">
+
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +63926-692-2954 </p>
+         <p> <i class="fas fa-phone"></i>  +63926-692-2954 </p>
+         <p> <i class="fas fa-envelope"></i> ndaus163@iskewla.psau.edu.ph </p>
+         <p> <i class="fas fa-map-marker-alt"></i> PSAU PAC,Magalang,Pampanga,Philippines </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>The 3 Kings</span> </p>
+
+</section>
